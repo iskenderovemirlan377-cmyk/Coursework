739b1d96f6c11654acbcc249f66baf22565d721d
iskenderovemirlan377-cmyk <iskenderovemirlan377@gmail.com>
2026-05-21 22:27:55 +0600

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    protected int id;
+    protected String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    public void display() {
+        System.out.println("ID: " + id + ", Name: " + name);
+    }
+}
